cbba1dbc47a2170653ee5256ef01fbde89f57579
raysofgoodness <shady5324@gmail.com>
2024-01-08 15:33:37 +0200

    added NewProject, Input components

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,19 @@
+import Input from "./Input.jsx";
+
+function NewProject() {
+    return (
+        <div>
+            <menu>
+                <li><button>Cancel</button></li>
+                <li><button>Save</button></li>
+            </menu>
+            <div>
+                <Input label="Title" />
+                <Input label="Description" textarea />
+                <Input label="Due Date" />
+            </div>
+        </div>
+    );
+}
+
+export default NewProject;
