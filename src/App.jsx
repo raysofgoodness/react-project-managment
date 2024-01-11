91412291a20e634efa1e794923c79d5c68c4ba20
raysofgoodness <shady5324@gmail.com>
2024-01-11 11:12:05 +0200

    added NoProjectSelected component, split to small components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Sidebar from "./components/Sidebar";
 import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
         <Sidebar />
-        <NewProject />
+        <NoProjectSelected />
     </main>
   );
 }
