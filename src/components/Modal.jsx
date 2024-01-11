69c20772ce4b8d8f0c40f0095ecf0d463c259484
raysofgoodness <shady5324@gmail.com>
2024-01-11 13:57:06 +0200

    added new function handleCancelAddProject

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,6 @@
 import { forwardRef, useImperativeHandle, useRef } from 'react';
 import { createPortal } from "react-dom";
+import Button from "./Button";
 
 const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
     const dialog = useRef();
@@ -13,10 +14,12 @@ const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
     });
 
     return createPortal(
-        <dialog ref={dialog}>
+        <dialog
+            ref={dialog}
+            className="backdrop:bg-stone-900/90 p-4 rounded-md shadow-md">
             {children}
-            <form method="dialog">
-                <button>{buttonCaption}</button>
+            <form className="mt-4 text-right" method="dialog">
+                <Button>{buttonCaption}</Button>
             </form>
         </dialog>,
         document.querySelector('#modal-root')
